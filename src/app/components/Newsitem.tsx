d9968e0860a480991da76a724ac50b538e0a92c5
imaihayato2025 <imaihayato2024@gmail.com>
2025-07-15 11:27:46 +0900

    fix: 画像をnext/imageに変更し、alt属性を追加

--- src/app/components/Newsitem.tsx
+++ src/app/components/Newsitem.tsx
@@ -1,7 +1,7 @@
 export default function Newsitem() {
   return (
    <div className="news-list h-[100px] flex p-2 border-[#e4e3e3] border-t border-b md:h-[150px]">
-      <div className="list-image w-[30%] bg-[#d9d9d9] "><img src="/path/to/image.jpg" alt="記事のサムネイル" /></div>
+      <div className="list-image w-[30%] bg-[#d9d9d9] "></div>
       <div className="list-text w-[70%] pl-2"><h2 className="text-[#23282f] text-[clamp(1rem,_0.667rem+1.67vw,_2rem)]">記事のタイトルが入ります。</h2></div>
    </div>
   );
